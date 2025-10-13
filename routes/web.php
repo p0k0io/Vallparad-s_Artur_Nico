82c26d07fd089609827e2918a6c36b2faba223ec
roro333111 <arturbartres@gmail.com>
2025-10-13 09:54:19 +0200

    artur 13.10.2025 merge amb problemes artur2 i main

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CenterController;
+use App\Http\Controllers\ProfessionalController;
+use App\Http\Controllers\CvController;
+use App\Http\Controllers\ProjectComisionController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('center',CenterController::class);
+Route::resource('professional',ProfessionalController::class);
+Route::resource('cv',CvController::class);
+Route::resource('projects_comisions',ProjectComisionController::class);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
